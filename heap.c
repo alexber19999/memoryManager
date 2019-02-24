f8cb8b31bc06dc4ac872e048ebfee23aa57de796
Alexander <alexber1999@gmail.com>
2019-02-23 20:21:03 -0500

    added helper functions initHeap, spaceFound, and insert to help with myMalloc

--- heap.c
+++ heap.c
@@ -9,8 +9,8 @@ typedef struct Node {
 	BloctType type;
 	size_t size;
 	char* startAddress;
-	struct Node* prev;
-	struct Node* next;
+	struct Node* prev = NULL;
+	struct Node* next = NULL;
 } Node;
 
 #define myHeap 1000
@@ -19,50 +19,88 @@ Node* head = NULL;
 
 void initHeap(){
 	heap = (char*) malloc(myHeap);
-	head = NULL;	
+	head = (Node*) malloc(heap);
+	head -> type = HOLE;
+	head -> startAddress = heap;
 }
+
+Node* spaceFound(size_t size){
+
+	Node* nextNode = head;
+
+	while(nextNode){
+
+		if(size >= nextNode -> size){
+			return nextNode;
+		}
+
+		nextNode = nextNode -> next;
+
+	}
+
+	return NULL;
+}
+
+Node* insert(size_t size){
+	Node* space = spaceFound(size, head);
+	if(!space){
+		return NULL;
+	}
+
+	if((head -> size = size) && (head -> type == HOLE)){
+		//add stuff for the first time to the heap
+		Node* newNode = (Node*) malloc(size);
+		
+	} else {
+		//add stuff in between 2 nodes
+	}
+}
+
 
 void* replacementAlloc(size_t size){
 	if(!heap){
 		initHeap();
 	}
 
-	if(!insert(head, size)){
+	Node* newNode = insert(head, size);
+
+	if(!newNode){
 		return NULL;
 	}
 
+	return newNode -> startAddress;
+
 
 }
-
 
 void replacementFree(void* address){
 
 }
 
-bool spaceFound(size_t size, Node* head){
-
-	size = head -> size;
-	Node* nextNode = head;
-
-	while(nextNode){
-
-		if(size >= nextNode -> size){
-			return true;
-		}
-
-	nextNode = nextNode -> next;
-
-	}
-
-	return false;
-}
-
-bool insert(Node* head, size_t size){
-	if(!spaceFound(size, head)){
-		return false;
-	}
 
 
 
-	//insert 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- replacementMalloc.h
+++ replacementMalloc.h
@@ -2,6 +2,7 @@
 #define replacementMalloc_h
 
 #include <stddef.h>
+#include "heap.h"
 
 void* myMalloc(size_t size);
 void myFree(void* address);
